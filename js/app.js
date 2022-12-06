f50e1e0deb062e4c33b0d2c6860072e616e0707d
Elian Lamouroux <99182600+laliane667@users.noreply.github.com>
2022-12-06 15:13:49 +0000

    Delete js directory

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLink = document.querySelector('.nav__container');
-
-menu.addEventListener('click', function(){
-    menu.classList.toggle('is-active');
-    menuLink.classList.toggle('active');
-});
